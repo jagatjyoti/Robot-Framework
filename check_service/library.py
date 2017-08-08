14dd49e4f782dcc0778b3fbd2c18009b8944d494
jagatjyoti <jagatjyoti0@gmail.com>
2017-08-08 02:53:52 -0700

    Working code

--- check_service/library.py
+++ check_service/library.py
@@ -12,7 +12,7 @@ def set_connection():
      s = paramiko.SSHClient()
      s.set_missing_host_key_policy(paramiko.AutoAddPolicy())
      print "Trying to connect to host ..."
-     s.connect('192.168.87.130', username='luckee', password='robot@123')
+     s.connect('68.128.156.119', username='zarvis', password='lucky@123')
      print "Connection Established !!"
  
   except KeyboardInterrupt:
@@ -41,8 +41,8 @@ def exec_command(cmd):
   output = stdout.readlines()
   print "out: " + str(output)
 
-  for o in output:
-  	print o
+#  for o in output:
+#  	print o
 
  
   e=stderr.readlines()
--- check_service/robot_library.py
+++ check_service/robot_library.py
@@ -3,10 +3,28 @@
 import subprocess
 import os
 import sys
+
+global tcid 
+tcid = ''
+
+#def tc_path(tcid):
+#	tc_path = os.getcwd() + tcid
+#        return tc_path
  
 def run_testcase(tcid):
-        cmd = 'python tcid'
-        p = subprocess.Popen(cmd, shell=True, stderr=subprocess.PIPE)
+        cmd = 'python ' + tcid  
+        p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         output, err = p.communicate()
         print output
         print err
+        if err != 0 and output == 0:
+            return False
+        else:
+            return True
+
+
+def get_result(tcid):
+	status = run_testcase(tcid)
+	if status == False: 
+	    print "Failed to execute " + tcid + ". Exiting ..."
+            sys.exit(1)
